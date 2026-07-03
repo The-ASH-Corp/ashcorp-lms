3eae87d1ee8ef7fd96d1fe5727be14aa74e09c37
shebin-tvk <shebintvk@gmail.com>
2026-07-03 17:15:52 +0530

    course detail page navigation added in course card

--- frontend/app/(protected)/layout.tsx
+++ frontend/app/(protected)/layout.tsx
@@ -43,10 +43,7 @@ export default function ProtectedLayout({
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 h-4"
-            />
+            <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
@@ -59,9 +56,7 @@ export default function ProtectedLayout({
               </BreadcrumbList>
             </Breadcrumb>
           </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            {children}
-          </div>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
         </SidebarInset>
       </SidebarProvider>
     </ProtectedRoute>
